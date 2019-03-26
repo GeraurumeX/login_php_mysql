210075405bcfdd33f00f9e5d24eb76bd025443aa
gerardo agustin <gerardoagustin@hotmail.com>
2019-03-26 20:04:42 -0300

    formulario de login

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Welcome to your App</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="./assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1>Please Login or SignUp</h1>
+
+    <a href="login.php">Login</a> or
+    <a href="signup.php">Signup</a>
+</body>
+
+</html> 
---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="./assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1>Login</h1>
+    <form action="login.php" method="post">
+        <input type="text" name="email" placeholder="Enter you email">
+        <input name="password" type="password" placeholder="Enter your Password">
+        <input type="submit" value="Send">
+    </form>
+</body>
+
+</html> 
